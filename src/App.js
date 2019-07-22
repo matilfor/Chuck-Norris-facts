a40a8f47c3f912c2dfa2e22a3661565c941df35b
Matilde Fortunato <matilde84@gmail.com>
2019-07-21 23:54:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fact: 'Chuck Norris can text using his walkie talkie and without batteries.'
+    };
+    
+    this.getFact = this.getFact.bind(this);
+  }
+  
+  getFact() {
+    axios.get('https://api.chucknorris.io/jokes/random')
+      .then(response => {
+      this.setState({fact: response.data.value});
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+  
+  render() {
+    return(
+      <div className="container text-center">
+        <h1 className="facts">Chuck Norris Facts</h1>
+        <h3>{'"' + this.state.fact + '"'}</h3>
+        <Button variant="dark" onClick={this.getFact}>More Chuck Facts!</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
